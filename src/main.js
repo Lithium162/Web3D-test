d47d19e4592750e07097fc48d286b8327d66ed55
Lithium162 <1130989834@qq.com>
2025-09-17 10:09:33 +0800

    test0.2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.170.0/build/three.module.min.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 //创建场景
 const scene =  new THREE.Scene();
 //创建相机视角
@@ -6,58 +7,140 @@ const camera = new THREE .PerspectiveCamera(75,window.innerWidth/window.innerHei
 camera.position.set(0, 8, 0);
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
+//
+
+
 //创建渲染器
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
-//创建物理信息
-const radius =  0.2; 
-const widthSegments = 30;
-const heightSegments = 30; 
-const geometrySun = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 
-//创建材质的纹理
+//创建纹理
 const loader1 = new THREE.TextureLoader();
 const texture1 = loader1.load('resources/images/test.png');
 const texture2 = loader1.load('resources/images/test.png');
+const texture3 = loader1.load('resources/images/test.png');
+const texture4 = loader1.load('resources/images/test.png');
+const texture5 = loader1.load('resources/images/test.png');
+const texture6 = loader1.load('resources/images/test.png');
+const texture7 = loader1.load('resources/images/test.png');
+const texture8 = loader1.load('resources/images/test.png');
+const texture9 = loader1.load('resources/images/test.png');
 //创建材质
-const material1 = new THREE.MeshBasicMaterial({map:texture1,});
-const material2 = new THREE.MeshBasicMaterial({map:texture2,});
+const material1 = new THREE.MeshBasicMaterial({map:texture1});
+const material2 = new THREE.MeshBasicMaterial({map:texture2});
+const material3 = new THREE.MeshBasicMaterial({map:texture3});
+const material4 = new THREE.MeshBasicMaterial({map:texture4});
+const material5 = new THREE.MeshBasicMaterial({map:texture5});
+const material6 = new THREE.MeshBasicMaterial({map:texture6});
+const material7 = new THREE.MeshBasicMaterial({map:texture7});
+const material8 = new THREE.MeshBasicMaterial({map:texture8});
+const material9 = new THREE.MeshBasicMaterial({map:texture9});
+
+//创建物理信息
+const data = {radius : 0.2,widthSegments : 30,heightSegments : 30 };
+const gplanet = new THREE.SphereGeometry(data.radius, data.widthSegments, data.heightSegments);
 
 //创建物体
 const solarSystem = new THREE.Object3D;
+const unit = 1;
 scene.add(solarSystem);
-const Sun = new THREE.Mesh(geometrySun,material1);
-Sun.scale.set(5, 5, 5);
-solarSystem.add(Sun);
+    const Sun = new THREE.Mesh(gplanet,material1);
+    Sun.scale.set(20, 20, 20);
+    solarSystem.add(Sun);
+//水
+    const MercuryOrbit = new THREE.Object3D();
+    const Mercury = new THREE.Mesh(gplanet,material2)
+    Mercury.scale.set(0.4, 0.4, 0.4);
+    Mercury.position.x = -0.4*unit;
+    MercuryOrbit.add(Mercury);
+//金
+    const VenusOrbit = new THREE.Object3D();
+    const Venus = new THREE.Mesh(gplanet,material3)
+    Venus.scale.set(1.2, 1.2, 1.2);
+    Venus.position.x = -0.7*unit;
+    VenusOrbit.add(Venus);
+//地
+    const earthOrbit = new THREE.Object3D();
+    const earth = new THREE.Mesh(gplanet,material4)
+    earth.scale.set(1.27, 1.27, 1.27);
+    earth.position.x = -1*unit;
+    earthOrbit.add(earth);
+        //月
+        const moonOrbit = new THREE.Object3D();
+        earth.add(moonOrbit);
+        const moon = new THREE.Mesh(gplanet,material2)
+        moon.position.x = 0.8;
+        moon.scale.set(0.5,0.5,0.5);
+        moonOrbit.add(moon);
+//火
+    const MarsOrbit = new THREE.Object3D();
+    const Mars = new THREE.Mesh(gplanet,material5)
+    Mars.scale.set(0.68, 0.68, 0.68);
+    Mars.position.x = -1.5*unit;
+    MarsOrbit.add(Mars);
 
+    const Asteroidbelt = new THREE.Object3D();
+//木
+    const JupiterOrbit = new THREE.Object3D();
+    const Jupiter = new THREE.Mesh(gplanet,material6)
+    Jupiter.scale.set(14, 14, 14);
+    Jupiter.position.x = -5.0*unit;
+    JupiterOrbit.add(Jupiter);
+//土
+    const SaturnOrbit = new THREE.Object3D();
+    const Saturn = new THREE.Mesh(gplanet,material7)
+    Saturn.scale.set(12, 12, 12);
+    Saturn.position.x = -10*unit;
+    SaturnOrbit.add(Saturn);
+        //环
+        const rings = new THREE.Object3D();
+//天
+    const UranusOrbit = new THREE.Object3D();
+    const Uranus = new THREE.Mesh(gplanet,material8)
+    Uranus.scale.set(5, 5, 5);
+    Uranus.position.x = -20*unit;
+    UranusOrbit.add(Uranus);
+//海
+    const NeptuneOrbit = new THREE.Object3D();
+    const Neptune = new THREE.Mesh(gplanet,material9)
+    Neptune.scale.set(5, 5, 5);
+    Neptune.position.x = -30*unit;
+    NeptuneOrbit.add(Neptune);
 
-const earthOrbit = new THREE.Object3D();
-earthOrbit.position.x = -3;
+solarSystem.add(MercuryOrbit);
+solarSystem.add(VenusOrbit);
 solarSystem.add(earthOrbit);
-const geometryEarth = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
-const earth = new THREE.Mesh(geometryEarth,material2)
-earthOrbit.add(earth);
-
-const geometryMoon = new THREE.SphereGeometry(radius,widthSegments,heightSegments);
-
-const moon = new THREE.Mesh(geometryEarth,material2)
-moon.position.x = 0.8;
-moon.scale.set(0.5,0.5,0.5);
-earthOrbit.add(moon);
-
+solarSystem.add(MarsOrbit);
+solarSystem.add(JupiterOrbit);
+solarSystem.add(SaturnOrbit);
+solarSystem.add(UranusOrbit);
+solarSystem.add(NeptuneOrbit);
 
 renderer.render(scene, camera);
+
+const speed =  1;
+function planetRevolution(){
+    solarSystem.rotation.y +=0.01*speed;
+    MercuryOrbit.rotation.y +=0.01*speed;
+    VenusOrbit.rotation.y +=0.01*speed;
+    earthOrbit.rotation.y +=0.005*speed;
+    MarsOrbit.rotation.y +=0.01*speed;
+    JupiterOrbit.rotation.y +=0.01*speed;
+    SaturnOrbit.rotation.y +=0.01*speed;
+    UranusOrbit.rotation.y +=0.01*speed;
+    NeptuneOrbit.rotation.y +=0.01*speed;
+}
 
 //动画函数
 function animate(){
     requestAnimationFrame(animate);
-
-    // solarSystem.rotation.x +=0.01;
-    solarSystem.rotation.y +=0.01;
-    earthOrbit.rotation.y +=0.005;
+    planetRevolution();
     renderer.render(scene, camera);
+    controls.update();
 }
 animate();
 
